b5e52054341d7c16f240d3e72df4f036d72474e4
metacomputingchoi <choi@metacomputing.co.kr>
2025-07-02 15:51:27 +0900

    prepare for profile(manager)/activity

---
+++ app/src/main/java/com/ssc/namespring/model/ProfileManager.kt
@@ -0,0 +1,13 @@
+package com.ssc.namespring.model
+
+object ProfileManager {
+    // profiles list
+    // get/set profiles
+    // functions around profile
+    // add/remove of profiles
+    // getCurrentProfile (selected profile이 있단뜻이고)
+    // switchProfile(idx? by name? ..)
+    // 어쨌든 프로필 관련 데이터 구조체 및 필요 기능 함수들이 필요하게 될듯.
+    // 중요한점: DFS해버려서 액티비티랑 엮지말자.(지금은)
+    // 저장까지(DataStore는 다음에)
+}
